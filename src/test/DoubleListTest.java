b31ef40a0db9c4b1af1a6bd41b03ff7c3aa86be7
SJTreadway <treadway08@gmail.com>
2018-04-06 01:03:29 -0500

    setUp and tearDown methods.

--- src/test/DoubleListTest.java
+++ src/test/DoubleListTest.java
@@ -6,14 +6,20 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import core.DoubleList;
+
 class DoubleListTest {
+	
+	private DoubleList<Integer> list;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		list = new DoubleList<>();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		list = null;
 	}
 
 	@Test
--- src/test/DoubleOrderedListTest.java
+++ src/test/DoubleOrderedListTest.java
@@ -6,14 +6,20 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import core.DoubleOrderedList;
+
 class DoubleOrderedListTest {
+	
+	private DoubleOrderedList<Integer> list;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		list = new DoubleOrderedList<>();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		list = null;
 	}
 
 	@Test
